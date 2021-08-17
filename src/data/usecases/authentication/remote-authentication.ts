ecec776b9d02bfff8579e0387bf123786ec61a69
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2021-08-16 23:14:36 -0300

    feat: ensure remote authentication throws UnexpectedError by default

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,4 +1,7 @@
 import { HttpPostClient } from "@/data/protocols/http/httpPostClient";
+import { HttpStatusCode } from "@/data/protocols/http/httpResponse";
+import { InvalidCredentialsError } from "@/domain/errors/InvalidCredentialsError";
+import { UnexpectedError } from "@/domain/errors/UnexpectedError";
 import { AuthenticationParams } from "@/domain/usecases/authentication";
 
 export class RemoteAuthentication {
@@ -8,9 +11,15 @@ export class RemoteAuthentication {
   ) { }
 
   async auth(params: AuthenticationParams): Promise<void> {
-    await this.httpPostClient.post({
+    const httpResponse = await this.httpPostClient.post({
       url: this.url,
       body: params
     })
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: break
+      case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
+      default: throw new UnexpectedError()
+    }
   }
 }
